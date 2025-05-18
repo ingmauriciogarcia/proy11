db3e99a30824daf7b56e61a157fd4a7af27dd60c
ingmauriciogarcia <mauricio.garcia.uped@gmail.com>
2025-05-18 12:44:43 -0600

    sexta version

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "¡Hola desde Azure con Entrega Continua Desde San Salvador1111!"
+    return "¡Hola mundo!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
